335670ef72bd5575c906cb2061773ec1efb567a3
Alejandro Reyes <alejandroereyes@gmail.com>
2015-05-16 15:01:33 -0500

    fixed bug in display card

--- lib/card.rb
+++ lib/card.rb
@@ -6,15 +6,16 @@ class Card
     @display_suit = check_suit
     @value = value
     @card_worth = blackjack_value
+    @display_value = display_value_adj
   end
 
   def display_card
     print "
-     ___________
-    |           |
-    | #{@display_suit}   |
-    | #{value}         |
-    |___________|"
+     ____________
+    |            |
+    |  #{@display_suit}   |
+    |     #{@display_value}     |
+    |____________|"
   end
 
   def check_suit
@@ -26,6 +27,14 @@ class Card
      return "♧ ♧ ♧ ♧"
     elsif @suit == :diamonds
       return "♢ ♢ ♢ ♢"
+    end
+  end
+
+  def display_value_adj
+    if @card_worth < 10 || @value == "J" ||  @value == "Q" || @value == "K" || @value == "A"
+      return "#{@value} "
+    else
+      return @value
     end
   end
 
